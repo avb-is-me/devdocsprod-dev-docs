f0bd987f6e0999e7c6538d579733fe4a05ffe64a
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2024-10-14 10:23:44 -0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,7 @@ import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
+  //odpfsiofjiusdhfuihdusifhuidshfuihdsuifhuidshfuihdsuihfuidhsuifhduishfuihdsufhudsgfudsf
+  //dsijfiodfhsuihfuidsfuiuiuifds
   return twMerge(clsx(inputs))
 }
